ac93508b0cf35364a6c8a76836fb58121d9607f4
vitaliy_m <miskotop1234@gmail.com>
2024-07-08 23:46:25 +0300

    The construction of the weather forecast component was implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import Forecast from './components/forecast/forecast';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api'
 import { useState } from 'react';
+
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -25,13 +27,15 @@ function App() {
     .catch((err) => console.log(err));
   }
 
-  console.log(currentWeather);
+
+  
   console.log(forecast)
 
   return (
     <div className="container">
      < Search onSearchChange={handleOnSearchChange}/>
     {currentWeather && < CurrentWeather data={currentWeather}/>}
+    {forecast && <Forecast data = {forecast}/>}
     </div>
   );
 }
--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -13,22 +13,22 @@ const CurrentWeather = ({data}) => {
             <div className="bottom">
                 <p className="temperature">{Math.round(data.main.temp)}°C</p>
                 <div className="details">
-                    <div className="peremeter-row">
+                    <div className="perameter-row">
                         <span className="parameter-label">Details</span>
                     </div>
-                    <div className="peremeter-row">
+                    <div className="perameter-row">
                         <span className="parameter-label">feels like</span>
                         <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
                     </div>
-                    <div className="peremeter-row">
+                    <div className="perameter-row">
                         <span className="parameter-label">Wind</span>
                         <span className="parameter-value">{data.wind.speed} m/s</span>
                     </div>
-                    <div className="peremeter-row">
+                    <div className="perameter-row">
                         <span className="parameter-label">Humidity</span>
                         <span className="parameter-value">{data.main.humidity} %</span>
                     </div>
-                    <div className="peremeter-row">
+                    <div className="perameter-row">
                         <span className="parameter-label">Pressure</span>
                         <span className="parameter-value">{data.main.pressure} hPa</span>
                     </div>
